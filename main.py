d02fee9a4c676b48ccf9f28ac9c798bb60e51733
DavidBSTRN <74006553+DavidBSTRN@users.noreply.github.com>
2023-02-19 19:47:54 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from pyrplidar import PyRPlidar
 import pygame
 import time
+import os
 from math import sin,cos,pi
 
 if __name__ == "__main__":
@@ -15,67 +16,62 @@ if __name__ == "__main__":
     health = lidar.get_health()
     print("health :", health)
 
-    samplerate = lidar.get_samplerate()
-    print("samplerate :", samplerate)
-    
-    
     
     # pygame init
     pygame.init()
     
-    width = 1200
-    height = 900
+    width = 800
+    height = 480
     screen = pygame.display.set_mode((width,height))
     
-    blue = (0,0,255)
-
-    clock = pygame.time.Clock()
+    black = (0,0,0)
+    white = (255,255,255)
+    red = (255,0,0)
     
-    #main loop
-    buffer = []#buffer with x and y coordinates
+    lets_continue = True
     
-    try:
-        lidar.set_motor_pwm(660)
-        time.sleep(2)
-    
-        scan_generator = lidar.start_scan_express(0)
-        
-        for count,scan in enumerate(scan_generator()):
+    while lets_continue:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                lets_continue = False
+                
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    try:
+                        coordinates = []
+                            
+                        lidar.set_motor_pwm(660)
+                        time.sleep(2)
+                            
+                        scan_generator = lidar.start_scan_express(0)
+                            
+                        for count,scan in enumerate(scan_generator()):
+                                
+                            angle = scan.angle
+                            distance = scan.distance
+                            
+                            scale = 15
+                                
+                            x = int((distance * cos(angle * pi/180)) / scale)
+                            y = int((distance * sin(angle * pi/180)) / scale)
+                                
+                            coordinates.append((x,y))
+                                
+                            if (count % 720) == 0:
             
-            for event in pygame.event.get():            
-                if event.type == pygame.QUIT:
-                    print(buffer)
-                    raise KeyboardInterrupt
-                
-            
-            if scan.quality != 0:             #ignore low quality data
-            
-                angle = scan.angle     #degree
-                distance = scan.distance    #mm
-                
-                scale = 10
-                
-                x = int((distance * cos(angle * pi/180)) / scale)      # x coordinate   
-                y = int((distance * sin(angle * pi/180)) / scale)      # y coordinate 
-                
-                buffer.append((x,y)) #add angle,distance to buffer
-                    
-                if (count % 720) == 0:
-                    
-                    screen.fill((255,255,255))          #white background
-                    
-                    for x,y in buffer:
-                        pygame.draw.circle(screen,blue,(int((width/2)+x),int((height/2)+y)),1)    
-                    
-                    pygame.display.update()
-                    buffer = []
-                
-     
-    except KeyboardInterrupt:               # ctrl-c
-        
-        pygame.quit()
-        lidar.stop()
-        lidar.set_motor_pwm(0)
-
-        
-        lidar.disconnect()
+                                screen.fill(black) 
+                                            
+                                for x,y in coordinates:
+                                    pygame.draw.circle(screen,white,(int((width/2)+x),int((height/2)+y)),1)
+                                    
+                                pygame.display.update()
+                                
+                                coordinates = []
+                                
+                    except KeyboardInterrupt:
+                        
+                        lidar.stop()
+                        lidar.set_motor_pwm(0)                                        
+                        lidar.disconnect()
+                        
+    pygame.quit()
