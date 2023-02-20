238b7cc3253c2431ec0526146aa21f830a38edab
DavidBSTRN <74006553+DavidBSTRN@users.noreply.github.com>
2023-02-20 23:49:30 +0100

    Update main.py

--- main.py
+++ main.py
@@ -8,14 +8,6 @@ if __name__ == "__main__":
     
     lidar = PyRPlidar()
     lidar.connect(port="/dev/ttyUSB0", baudrate=256000, timeout=3) 
-
-                  
-    info = lidar.get_info()
-    print("info :", info)
-
-    health = lidar.get_health()
-    print("health :", health)
-
     
     # pygame init
     pygame.init()
@@ -34,44 +26,41 @@ if __name__ == "__main__":
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 lets_continue = False
-                
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_UP:
-                    try:
-                        coordinates = []
-                            
-                        lidar.set_motor_pwm(660)
-                        time.sleep(2)
-                            
-                        scan_generator = lidar.start_scan_express(0)
-                            
-                        for count,scan in enumerate(scan_generator()):
+                coordinates = []
                                 
-                            angle = scan.angle
-                            distance = scan.distance
-                            
-                            scale = 15
+                lidar.set_motor_pwm(660)
+                time.sleep(2)
                                 
-                            x = int((distance * cos(angle * pi/180)) / scale)
-                            y = int((distance * sin(angle * pi/180)) / scale)
+                scan_generator = lidar.start_scan_express(0)
                                 
-                            coordinates.append((x,y))
-                                
-                            if (count % 720) == 0:
-            
-                                screen.fill(black) 
-                                            
-                                for x,y in coordinates:
-                                    pygame.draw.circle(screen,white,(int((width/2)+x),int((height/2)+y)),1)
+                for scan in scan_generator():
                                     
-                                pygame.display.update()
+                    angle = scan.angle
+                    distance = scan.distance
                                 
-                                coordinates = []
+                    scale = 15
+                                    
+                    x = int((distance * cos(angle * pi/180)) / scale)
+                    y = int((distance * sin(angle * pi/180)) / scale)
+                                    
+                    coordinates.append((x,y))
+                                    
+                    if len(coordinates) >= 720:
+                
+                        screen.fill(black) 
+                                                
+                        for x,y in coordinates:
+                            pygame.draw.circle(screen,white,(int((width/2)+x),int((height/2)+y)),1)
+                                        
+                        pygame.display.update()
+                                    
+                        coordinates = []
+                        break
+                        
+                lidar.stop()
+                lidar.set_motor_pwm(0)
+    
+    lidar.disconnect()
                                 
-                    except KeyboardInterrupt:
-                        
-                        lidar.stop()
-                        lidar.set_motor_pwm(0)                                        
-                        lidar.disconnect()
-                        
     pygame.quit()
